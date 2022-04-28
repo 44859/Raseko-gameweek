38c35a201c45661735835c71e03dc3125a9698db
AapoLineri <91244583+AapoLineri@users.noreply.github.com>
2022-04-28 11:11:33 +0300

    Add files via upload
    Lisätty javascript.

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("mobile-menu");
+const menuLink = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function() {
+menu.lassList.toggle("is.active");
+menulinks.classList.toggle("active");
+}
